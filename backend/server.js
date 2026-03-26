f50484078509e56b4d011665fd6f58b69e960793
sri vaishnavi <vaishnavi@sris-MacBook-Air.local>
2026-03-26 21:40:10 +0530

    Add static file serving to fix frontend connection issues

--- backend/server.js
+++ backend/server.js
@@ -18,6 +18,7 @@ app.use(cors({
     allowedHeaders: ['Content-Type', 'Authorization']
 }));
 app.use(express.json());
+app.use(express.static(__dirname + '/..'));
 
 // Database connection pool
 const db = mysql.createPool({
